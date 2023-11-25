f419c7a7b59f58dc4f57fcd7562b099cf15d5729
Nikola Ištvanić <117486054+nikola01pz@users.noreply.github.com>
2023-11-25 12:07:07 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,30 @@
+<?php
+$host = "ruaplv-ni-server.mysql.database.azure.com";
+$user = "uooqgxjjko";
+$pwd = "23CIJ806GF3567AZ$";
+$db = "ruaplv-ni-database";
+ // $host = "127.0.0.1:52932";
+ // $user = "azure";
+ // $pwd = "6#vWHD_$";
+ // $db = "localdb";
+ $conn = mysqli_connect($host,$user,$pwd);
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ else {
+ echo 'Connected successfully';
+ }
+ 
+ mysqli_select_db($conn,$db);
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+ AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+ VARCHAR(30), date DATE);";
+ if ($conn->query($sql_create) === TRUE) {
+ echo "Table registration_tbl created successfully";
+ } else {
+ echo "Error creating table: " . $conn->error;
+ }
+ $conn->close();
+?>
